90ceee84f41f0a7440d02cd07b103be5d8b1fd91
Mateone <54219667+Mateone@users.noreply.github.com>
2021-06-24 20:17:59 -0300

    Pasaron los tests

--- TA8/test/CumpleAVLTest.java
+++ TA8/test/CumpleAVLTest.java
@@ -16,33 +16,40 @@ public class CumpleAVLTest {
     TElementoAB<String> elem1;
     TElementoAB<String> elem8;
     TElementoAB<String> elem6;
-    TElementoAB<String> elem7;
+    TElementoAB<String> elem9;
+    TElementoAB<String> elem10;
+    TElementoAB<String> elem11;
+    TElementoAB<String> elem12;
+    TElementoAB<String> elem14;
 
     @BeforeEach
     public void setUp() throws Exception {
-        
+
         arbolito = new TArbolBB<>();
         vacio = new TArbolBB<>();
         arbolitoQueNoCumple = new TArbolBB<>();
 
-        elem4 = new TElementoAB<>(4,"");
-        elem1 = new TElementoAB<>(1,"");
-        elem6 = new TElementoAB<>(6,"");
-        elem8 = new TElementoAB<>(8,"");
+        elem4 = new TElementoAB<>("4", "");
+        elem1 = new TElementoAB<>("1", "");
+        elem6 = new TElementoAB<>("6", "");
+        elem8 = new TElementoAB<>("8", "");
 
-        elem7 = new TElementoAB<>(7,"");
-        
+        elem9 = new TElementoAB<>("9", "");
+        elem10 = new TElementoAB<>("10", "");
+        elem11 = new TElementoAB<>("11", "");
+        elem12 = new TElementoAB<>("12", "");
+        elem14 = new TElementoAB<>("14", "");
+
         arbolito.insertar(elem4);
         arbolito.insertar(elem1);
         arbolito.insertar(elem6);
         arbolito.insertar(elem8);
 
-        arbolitoQueNoCumple.insertar(elem4);
-        arbolitoQueNoCumple.insertar(elem1);
-        arbolitoQueNoCumple.insertar(elem6);
-        arbolitoQueNoCumple.insertar(elem8);
-
-        arbolitoQueNoCumple.insertar(elem7);
+        arbolitoQueNoCumple.insertar(elem10);
+        arbolitoQueNoCumple.insertar(elem11);
+        arbolitoQueNoCumple.insertar(elem12);
+        arbolitoQueNoCumple.insertar(elem14);
+        arbolitoQueNoCumple.insertar(elem9);
     }
 
     @Test
